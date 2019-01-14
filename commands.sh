af69a59305657de158a258816a205f9d0c82c0e2
abhi <you@example.com>
2019-01-14 19:40:43 +0530

    new command 2

--- commands.sh
+++ commands.sh
@@ -3,3 +3,8 @@ sudo hdparm -Tt /dev/sda
 
 # Get information on disk write information
 sync; dd if=/dev/zero of=tempfile bs=1M count=1024; sync
+
+# view hardware topo
+sudo apt-get install hwloc
+
+lstopo 
